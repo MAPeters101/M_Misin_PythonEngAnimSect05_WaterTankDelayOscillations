9965e1838f729c315a55aa907f0c860172ca00f8
Mark Peters <mapeters101@earthlink.net>
2024-10-29 10:46:48 -0400

    Lesson091:  Complete lesson.

---
+++ python_waterTanks.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib.animation as animation
+import numpy as np
+
+# Time array
+t0=0 # [s]
+t_end=60 # [s]
+dt=0.04
+t=np.arange(t0,t_end+dt,dt)
+print(len(t))
+# Create arrays for motion
+
