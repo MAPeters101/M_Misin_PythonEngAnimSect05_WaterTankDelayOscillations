7e27b8417e6a556fd07bb6be5643b55609a33bd7
Mark Peters <mapeters101@earthlink.net>
2024-10-29 11:06:23 -0400

    Lesson093:  Complete lesson.

--- python_waterTanks.py
+++ python_waterTanks.py
@@ -8,6 +8,60 @@ t0=0 # [s]
 t_end=60 # [s]
 dt=0.04
 t=np.arange(t0,t_end+dt,dt)
-print(len(t))
-# Create arrays for motion
+
+# Zero arrays for the tanks' volumes
+volume_Tank1=np.zeros(len(t))
+volume_Tank2=np.zeros(len(t))
+volume_Tank3=np.zeros(len(t))
+
+# Create volumes for
+for i in range(0,len(t)):
+    # Tank 1
+    if t[i]<=22.5:
+        volume_Tank1[i]=50+2*t[i]
+    elif t[i]<=32.5:
+        volume_Tank1[i]=95
+        temp11=i
+    elif t[i]<=32.5+45**0.5:
+        volume_Tank1[i]=95-(t[i]-t[temp11])**2
+        temp12=i
+    elif t[i]<=42.5+45**0.5:
+        volume_Tank1[i]=50+np.sin(2*np.pi*1*(t[i]-t[temp12]))
+    else:
+        volume_Tank1[i]=50
+
+    # Tank 2
+    if t[i]<=27.5:
+        volume_Tank2[i]=40+2*t[i]
+    elif t[i]<=32.5:
+        volume_Tank2[i]=95
+        temp21=i
+    elif t[i]<=32.5+45**0.5:
+        volume_Tank2[i]=95-(t[i]-t[temp21])**2
+        temp22=i
+    elif t[i]<=37.5+45**0.5:
+        volume_Tank2[i]=50+3*np.sin(2*np.pi*1*(t[i]-t[temp22]))
+        temp23=i
+    elif t[i]<=42.5+45**0.5:
+        volume_Tank2[i]=50+np.sin(2*np.pi*1*(t[i]-t[temp23]))
+    else:
+        volume_Tank2[i]=50
+
+    # Tank 3
+    if t[i]<=27.5:
+        volume_Tank3[i]=30+2*t[i]
+        temp31=i
+    elif t[i]<=32.5+45**0.5:
+        volume_Tank3[i]=95-(t[i]-t[temp31])**2
+        temp32=i
+    elif t[i]<=42.5+45**0.5:
+        volume_Tank3[i]=50-np.sin(2*np.pi*1*(t[i]-t[temp32]))
+    else:
+        volume_Tank3[i]=50
+
+
+
+
+
+
 
